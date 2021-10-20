25553ba6b24661d461aeed3ad6c1ff9a86903c39
K. M <90254516+km1610@users.noreply.github.com>
2021-10-20 12:10:40 +0530

    Update script.js

--- Module4-Solution/script.js
+++ Module4-Solution/script.js
@@ -2,9 +2,9 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 
 for (var i = 0; i < names.length; i++) {
 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
+        	console.log("Goodbye "+ names[i]);
 	}
 	else{
-		console.log("Hello "+ names[i])
+		console.log("Hello "+ names[i]);
 	}
 }
